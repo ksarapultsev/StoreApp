afb1085609e89e22449d3d6fb90a69a4c34330e2
ksarapultsev <ksarapultsev2014@gmail.com>
2020-05-02 11:29:31 +0600

    Добавьте файлы проекта.

---
+++ StoreApp/Models/Groups.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApp.Models
+{
+    public class Groups
+    {
+        public long groupId { get; set; }
+        public string groupName { get; set; }
+
+    }
+}
